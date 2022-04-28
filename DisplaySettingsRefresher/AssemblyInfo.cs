b5ad8e1829c25ee12ae8cf71823f6fd5c93f1bb4
hb226 <78278844+hb226@users.noreply.github.com>
2022-04-27 22:28:43 -0700

    Add files via upload

---
+++ DisplaySettingsRefresher/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+
+// Do not change the entries below!
+#if X64
+[assembly: AssemblyInformationalVersion("3.0.2.2161 (64-bit)")]
+#else
+[assembly: AssemblyInformationalVersion("3.0.2.2161 (32-bit)")]
+#endif
+[assembly: AssemblyProduct("Rainmeter")]
+[assembly: AssemblyTitle("DisplaySettingsRefresher")]
+[assembly: AssemblyDescriptionD("Display Settings Changed Event Handler for Rainmeter")]
+[assembly: Guid("744dbd15-cd0b-4dfc-84c2-a663853d1d14")]
---
+++ DisplaySettingsRefresher/DisplaySettingsRefresher.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+using Rainmeter;
+
+namespace DisplaySettingsRefresher
+{
+    class Measure
+    {
+        public static implicit operator Measure(IntPtr data)
+        {
+            return (Measure)GCHandle.FromIntPtr(data).Target;
+        }
+        public IntPtr buffer = IntPtr.Zero;
+    }
+
+    public class Plugin
+    {
+        private static EventHandler _displayChangedHandler;
+        private static string _command;
+        private static void DisplayChanged(API api)
+        {
+            api.Execute(_command);
+        }
+        
+        [DllExport]
+        public static void Initialize(ref IntPtr data, IntPtr rm)
+        {
+            data = GCHandle.ToIntPtr(GCHandle.Alloc(new Measure()));
+            
+            API api = rm;
+            
+            _command = api.ReadString("Command", "[!RefreshApp]");
+            
+            _displayChangedHandler = delegate
+            {
+                DisplayChanged(api);
+            };
+            
+            SystemEvents.DisplaySettingsChanged += _displayChangedHandler;
+        }
+
+        [DllExport]
+        public static void Finalize(IntPtr data)
+        {
+            SystemEvents.DisplaySettingsChanged -= _displayChangedHandler;
+            
+            Measure measure = data;
+            
+            if (measure.buffer != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(measure.buffer);
+            }
+            
+            GCHandle.FromIntPtr(data).Free();
+        }
+
+        [DllExport]
+        public static void Reload(IntPtr data, IntPtr rm, ref double maxValue)
+        {
+        }
+
+        [DllExport]
+        public static double Update(IntPtr data) => 0.0;
+    }
+}
+
